8083b7617f0192e659da9f9a47012eb385fef909
krolakb <krolakbartlomiej@gmail.com>
2020-03-13 19:06:41 +0100

    Refactor grammar checker

--- src/main/java/parser/GrammarCheckerImpl.java
+++ src/main/java/parser/GrammarCheckerImpl.java
@@ -6,7 +6,6 @@ import token.TokenType;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 public class GrammarCheckerImpl implements GrammarChecker {
 
@@ -18,14 +17,10 @@ public class GrammarCheckerImpl implements GrammarChecker {
     }
 
     private boolean checkFile(List<Token> tokens) {
-        List<Token> copyOfTokenList = new ArrayList<>(tokens);
-        if (checkTerminalToken(copyOfTokenList, TokenType.OPEN_BRACE) && checkBody(copyOfTokenList)
-                && checkTerminalToken(copyOfTokenList, TokenType.CLOSE_BRACE)) {
-            tokens.clear();
-            tokens.addAll(copyOfTokenList);
+        if (checkTokens(List.of(this::checkOpenBrace, this::checkBody, this::checkCloseBrace), tokens)) {
             return true;
         }
-        throw new ParsingException(copyOfTokenList, this.tokens);
+        throw new ParsingException(tokens, this.tokens);
     }
 
     private boolean checkBody(List<Token> tokens) {
@@ -94,126 +89,152 @@ public class GrammarCheckerImpl implements GrammarChecker {
         return correctGrammar;
     }
 
+    private boolean checkCloseBrace(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.CLOSE_BRACE);
+    }
+
+    private boolean checkOpenBrace(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.OPEN_BRACE);
+    }
+
+    private boolean checkRequiredKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.REQUIRED_KW);
+    }
+
+    private boolean checkTitleKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.TITLE_KW);
+    }
+
+    private boolean checkDefinitionsKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.DEFINITIONS_KW);
+    }
+
+    private boolean checkSchemaKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.SCHEMA_KW);
+    }
+
+    private boolean checkIdKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.ID_KW);
+    }
+
+    private boolean checkTypeKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.TYPE_KW);
+    }
+
+    private boolean checkDescriptionKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.DESCRIPTION_KW);
+    }
+
+    private boolean checkPropertiesKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.PROPERTIES_KW);
+    }
+
+    private boolean checkEnumKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.ENUM_KW);
+    }
+
+    private boolean checkMinimumKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.MINIMUM_KW);
+    }
+
+    private boolean checkMaximumKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.MAXIMUM_KW);
+    }
+
+    private boolean checkMinLengthKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.MIN_LENGTH_KW);
+    }
+
+    private boolean checkMaxLengthKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.MAX_LENGTH_KW);
+    }
+
+    private boolean checkRefKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.REF_KW);
+    }
+
+    private boolean checkColonKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.COLON);
+    }
+
+    private boolean checkStringKeyword(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.STRING_KW);
+    }
+
+    private boolean checkString(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.STRING);
+    }
+
+    private boolean checkOpenBracket(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.OPEN_BRACKET);
+    }
+
+    private boolean checkCloseBracket(List<Token> tokens) {
+        return checkTerminalToken(tokens, TokenType.CLOSE_BRACKET);
+    }
+
     private boolean checkMaxLength(List<Token> tokens) {
-        List<Token> copyOfTokenList = new ArrayList<>(tokens);
-        if (checkTerminalToken(copyOfTokenList, TokenType.MAX_LENGTH_KW) && checkTerminalToken(copyOfTokenList, TokenType.COLON)
-                && checkNumber(copyOfTokenList)) {
-            tokens.clear();
-            tokens.addAll(copyOfTokenList);
-            return true;
-        }
-        return false;
+        return checkTokens(List.of(this::checkMaxLengthKeyword, this::checkColonKeyword, this::checkNumber), tokens);
     }
 
     private boolean checkMinLength(List<Token> tokens) {
-        List<Token> copyOfTokenList = new ArrayList<>(tokens);
-        if (checkTerminalToken(copyOfTokenList, TokenType.MIN_LENGTH_KW) && checkTerminalToken(copyOfTokenList, TokenType.COLON)
-                && checkNumber(copyOfTokenList)) {
-            tokens.clear();
-            tokens.addAll(copyOfTokenList);
-            return true;
-        }
-        return false;
+        return checkTokens(List.of(this::checkMinLengthKeyword, this::checkColonKeyword, this::checkNumber), tokens);
     }
 
     private boolean checkRefEntry(List<Token> tokens) {
-        List<Token> copyOfTokenList = new ArrayList<>(tokens);
-        if (checkTerminalToken(copyOfTokenList, TokenType.REF_KW) && checkTerminalToken(copyOfTokenList, TokenType.COLON)
-                && checkTerminalToken(copyOfTokenList, TokenType.STRING)) {
-            tokens.clear();
-            tokens.addAll(copyOfTokenList);
-            return true;
-        }
-        return false;
+        return checkTokens(List.of(this::checkRefKeyword, this::checkColonKeyword, this::checkString), tokens);
     }
 
     private boolean checkMaximumEntry(List<Token> tokens) {
-        List<Token> copyOfTokenList = new ArrayList<>(tokens);
-        if (checkTerminalToken(copyOfTokenList, TokenType.MINIMUM_KW) && checkTerminalToken(copyOfTokenList, TokenType.COLON)
-                && checkNumber(copyOfTokenList)) {
-            tokens.clear();
-            tokens.addAll(copyOfTokenList);
-            return true;
-        }
-        return false;
+        return checkTokens(List.of(this::checkMaximumKeyword, this::checkColonKeyword, this::checkNumber), tokens);
     }
 
     private boolean checkMinimumEntry(List<Token> tokens) {
-        List<Token> copyOfTokenList = new ArrayList<>(tokens);
-        if (checkTerminalToken(copyOfTokenList, TokenType.MINIMUM_KW) && checkTerminalToken(copyOfTokenList, TokenType.COLON)
-                && checkNumber(copyOfTokenList)) {
-            tokens.clear();
-            tokens.addAll(copyOfTokenList);
-            return true;
-        }
-        return false;
+        return checkTokens(List.of(this::checkMinimumKeyword, this::checkColonKeyword, this::checkNumber), tokens);
     }
 
     private boolean checkEnumEntry(List<Token> tokens) {
-        List<Token> copyOfTokenList = new ArrayList<>(tokens);
-        if (checkTerminalToken(copyOfTokenList, TokenType.ENUM_KW) && checkTerminalToken(copyOfTokenList, TokenType.COLON)
-                && checkStringArray(copyOfTokenList)) {
-            tokens.clear();
-            tokens.addAll(copyOfTokenList);
-            return true;
-        }
-        return false;
+        return checkTokens(List.of(this::checkEnumKeyword, this::checkColonKeyword, this::checkStringArray), tokens);
     }
 
     private boolean checkDescriptionEntry(List<Token> tokens) {
-        List<Token> copyOfTokenList = new ArrayList<>(tokens);
-        if (checkTerminalToken(copyOfTokenList, TokenType.DESCRIPTION_KW) && checkTerminalToken(copyOfTokenList, TokenType.COLON)
-                && checkTerminalToken(copyOfTokenList, TokenType.STRING)) {
-            tokens.clear();
-            tokens.addAll(copyOfTokenList);
-            return true;
-        }
-        return false;
-    }
-
-    private boolean checkDefinitionEntry(List<Token> tokens) {
-        List<Token> copyOfTokenList = new ArrayList<>(tokens);
-        if (checkTerminalToken(copyOfTokenList, TokenType.DEFINITIONS_KW) && checkTerminalToken(copyOfTokenList, TokenType.COLON)
-                && checkFile(copyOfTokenList)) {
-            tokens.clear();
-            tokens.addAll(copyOfTokenList);
-            return true;
-        }
-        return false;
-    }
-
-
-    private boolean checkStringEntry(List<Token> tokens) {
-        List<Token> copyOfTokenList = new ArrayList<>(tokens);
-        if (checkTerminalToken(copyOfTokenList, TokenType.STRING) && checkTerminalToken(copyOfTokenList, TokenType.COLON)
-                && checkFile(copyOfTokenList)) {
-            tokens.clear();
-            tokens.addAll(copyOfTokenList);
-            return true;
-        }
-        return false;
-    }
-
-    private boolean checkPropertiesEntry(List<Token> tokens) {
-        List<Token> copyOfTokenList = new ArrayList<>(tokens);
-        if (checkTerminalToken(copyOfTokenList, TokenType.PROPERTIES_KW) && checkTerminalToken(copyOfTokenList, TokenType.COLON)
-                && checkFile(copyOfTokenList)) {
-            tokens.clear();
-            tokens.addAll(copyOfTokenList);
-            return true;
-        }
-        return false;
+        return checkTokens(List.of(this::checkDescriptionKeyword, this::checkColonKeyword, this::checkString), tokens);
     }
 
     private boolean checkTypeEntry(List<Token> tokens) {
-        List<Token> copyOfTokenList = new ArrayList<>(tokens);
-        if (checkTerminalToken(copyOfTokenList, TokenType.TYPE_KW) && checkTerminalToken(copyOfTokenList, TokenType.COLON)
-                && checkTypeBody(copyOfTokenList)) {
-            tokens.clear();
-            tokens.addAll(copyOfTokenList);
-            return true;
-        }
-        return false;
+        return checkTokens(List.of(this::checkTypeKeyword, this::checkColonKeyword, this::checkTypeBody), tokens);
+    }
+
+    private boolean checkDefinitionEntry(List<Token> tokens) {
+        return checkTokens(List.of(this::checkDefinitionsKeyword, this::checkColonKeyword, this::checkFile), tokens);
+    }
+
+    private boolean checkIdEntry(List<Token> tokens) {
+        return checkTokens(List.of(this::checkIdKeyword, this::checkColonKeyword, this::checkString), tokens);
+    }
+
+    private boolean checkSchemaEntry(List<Token> tokens) {
+        return checkTokens(List.of(this::checkSchemaKeyword, this::checkColonKeyword, this::checkString), tokens);
+    }
+
+    private boolean checkTitleEntry(List<Token> tokens) {
+        return checkTokens(List.of(this::checkTitleKeyword, this::checkColonKeyword, this::checkString), tokens);
+    }
+
+    private boolean checkRequiredEntry(List<Token> tokens) {
+        return checkTokens(List.of(this::checkRequiredKeyword, this::checkColonKeyword, this::checkStringArray), tokens);
+    }
+
+    private boolean checkStringArray(List<Token> tokens) {
+        return checkTokens(List.of(this::checkOpenBracket, this::checkMultipleStrings, this::checkCloseBracket), tokens);
+    }
+
+    private boolean checkStringEntry(List<Token> tokens) {
+        return checkTokens(List.of(this::checkString, this::checkColonKeyword, this::checkFile), tokens);
+    }
+
+    private boolean checkPropertiesEntry(List<Token> tokens) {
+        return checkTokens(List.of(this::checkPropertiesKeyword, this::checkColonKeyword, this::checkFile), tokens);
     }
 
     private boolean checkTypeBody(List<Token> tokens) {
@@ -221,40 +242,6 @@ public class GrammarCheckerImpl implements GrammarChecker {
                 || checkTerminalToken(tokens, TokenType.NUMBER_KW) || checkTerminalToken(tokens, TokenType.STRING_KW)
                 || checkTerminalToken(tokens, TokenType.NULL_KW) || checkTerminalToken(tokens, TokenType.ARRAY_KW)
                 || checkTerminalToken(tokens, TokenType.BOOLEAN_KW);
-    }
-
-    private boolean checkIdEntry(List<Token> tokens) {
-        return checkTerminalTokens(tokens, List.of(TokenType.ID_KW, TokenType.COLON, TokenType.STRING));
-    }
-
-    private boolean checkSchemaEntry(List<Token> tokens) {
-        return checkTerminalTokens(tokens, List.of(TokenType.SCHEMA_KW, TokenType.COLON, TokenType.STRING));
-    }
-
-    private boolean checkTitleEntry(List<Token> tokens) {
-        return checkTerminalTokens(tokens, List.of(TokenType.TITLE_KW, TokenType.COLON, TokenType.STRING));
-    }
-
-    private boolean checkRequiredEntry(List<Token> tokens) {
-        List<Token> copyOfTokenList = new ArrayList<>(tokens);
-        if (checkTerminalToken(copyOfTokenList, TokenType.REQUIRED_KW) && checkTerminalToken(copyOfTokenList, TokenType.COLON)
-                && checkStringArray(copyOfTokenList)) {
-            tokens.clear();
-            tokens.addAll(copyOfTokenList);
-            return true;
-        }
-        return false;
-    }
-
-    private boolean checkStringArray(List<Token> tokens) {
-        List<Token> copyOfTokenList = new ArrayList<>(tokens);
-        if (checkTerminalToken(copyOfTokenList, TokenType.OPEN_BRACKET) && checkMultipleStrings(copyOfTokenList)
-                && checkTerminalToken(copyOfTokenList, TokenType.CLOSE_BRACKET)) {
-            tokens.clear();
-            tokens.addAll(copyOfTokenList);
-            return true;
-        }
-        return false;
     }
 
     private boolean checkMultipleStrings(List<Token> tokens) {
@@ -320,16 +307,14 @@ public class GrammarCheckerImpl implements GrammarChecker {
         return allCorrect;
     }
 
-    private boolean checkTerminalTokens(List<Token> tokens, List<TokenType> grammar) {
-        if (tokens.stream()
-                .map(Token::getTokenType)
-                .collect(Collectors.toList())
-                .subList(0, grammar.size())
-                .equals(grammar)) {
-            tokens.subList(0, grammar.size()).clear();
-            return true;
+    private boolean checkTokens(List<Predicate<List<Token>>> checkers, List<Token> tokens) {
+        List<Token> copy = new ArrayList<>(tokens);
+        boolean correctGrammar = checkers.stream().allMatch(checker -> checker.test(copy));
+        if (correctGrammar) {
+            tokens.clear();
+            tokens.addAll(copy);
         }
-        return false;
+        return correctGrammar;
     }
 
     private boolean checkTerminalToken(List<Token> tokens, TokenType tokenType) {
